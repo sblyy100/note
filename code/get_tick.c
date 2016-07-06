57f85392ab1fdef15207a16cf83cc44a9ee11bf7
unknown <sunbaoliang@intra.legendsec.com>
2016-07-06 14:16:08 +0800

    tick

---
+++ code/get_tick.c
@@ -0,0 +1,16 @@
+static inline uint64_t
+rte_rdtsc(void)
+{
+	union {
+		uint64_t tsc_64;
+		struct {
+			uint32_t lo_32;
+			uint32_t hi_32;
+		};
+	} tsc;
+
+	asm volatile("rdtsc" :
+		     "=a" (tsc.lo_32),
+		     "=d" (tsc.hi_32));
+	return tsc.tsc_64;
+}
